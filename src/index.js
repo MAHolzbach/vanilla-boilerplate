733b0a2b1bd0ffeeaa118f28af0e208d6d614b2f
Mike Holzbach <mikeholzbach@a-w3595htd6.carfax.cfx>
2018-06-01 14:16:24 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+if (module.hot) module.hot.accept();
+
+// JS
+import './components/landing/landing';
+
+// Styles
+import './components/landing/landing.scss';
---
+++ webpack.config.babel.js
@@ -0,0 +1,89 @@
+import { resolve } from 'path';
+import webpack from 'webpack';
+import exec from 'script-loader';
+import UglifyJsPlugin from 'uglifyjs-webpack-plugin';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
+
+module.exports = (env, argv) => {
+  const isDev = argv.mode === 'development';
+
+  return {
+    context: resolve(__dirname, 'src'),
+    entry: './index.js',
+    output: {
+      path: resolve(__dirname, 'dist'),
+      filename: 'bundle-[hash].js'
+    },
+    devServer: {
+      contentBase: 'src',
+      hot: true
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(scss|css)$/,
+          loader: ExtractTextPlugin.extract({
+            fallback: { loader: 'style-loader' },
+            use: [
+              {
+                loader: 'css-loader',
+                options: {
+                  importLoaders: 1
+                }
+              },
+              { loader: 'postcss-loader' },
+              {
+                loader: 'sass-loader'
+              }
+            ]
+          })
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader'
+        },
+        {
+          test: /\.exec\.js$/,
+          use: ['script-loader']
+        },
+        {
+          test: /\.(gif|png|jpe?g|svg)$/i,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: '[path][name]-[hash].[ext]'
+              }
+            },
+            {
+              loader: 'image-webpack-loader',
+              options: {
+                bypassOnDebug: true
+              }
+            }
+          ]
+        },
+        { test: /\.pug$/, loader: 'pug-loader' }
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: 'index.pug',
+        minify: {
+          removeComments: true,
+          collapseWhitespace: true
+        }
+      }),
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.NamedModulesPlugin(),
+      new UglifyJsPlugin({
+        uglifyOptions: {
+          keep_fnames: true
+        }
+      }),
+      new ExtractTextPlugin({ filename: 'styles-[hash].css', disable: isDev })
+    ]
+  };
+};
